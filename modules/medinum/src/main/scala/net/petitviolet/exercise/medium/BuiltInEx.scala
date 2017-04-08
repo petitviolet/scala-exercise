82fdeae3d1723578adb74c30f1d5fc5b4609afa1
petitviolet <violethero0820@gmail.com>
2017-04-08 23:32:06 +0900

    add composeMonad problem

--- modules/medinum/src/main/scala/net/petitviolet/exercise/medium/BuiltInEx.scala
+++ modules/medinum/src/main/scala/net/petitviolet/exercise/medium/BuiltInEx.scala
@@ -1,6 +1,7 @@
 package net.petitviolet.exercise.medium
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
 
 /**
  * exercise about built-in classes
@@ -9,11 +10,23 @@ trait BuiltInEx {
   final private def sleepInt(n: Int)(implicit ec: ExecutionContext): Future[Int] =
     Future { Thread.sleep(n * 100); n }
 
-  final protected def getFirstFinished(i: Int, j: Int)(implicit ec: ExecutionContext): Int = {
+  final def getFirstFinished(i: Int, j: Int)(implicit ec: ExecutionContext): Int = {
     val iSleep = sleepInt(i)
     val jSleep = sleepInt(j)
     firstFinishFuture(iSleep, jSleep)
   }
 
-  def firstFinishFuture(iF: Future[Int], jF: Future[Int])(implicit ec: ExecutionContext): Int
+  protected def firstFinishFuture(iF: Future[Int], jF: Future[Int])(implicit ec: ExecutionContext): Int
+
+  /**
+   * compose Int of Try, Option, Either and Seq
+   * if any of monad fails, return 0
+   * else return multiple all values
+   * @param t
+   * @param o
+   * @param e
+   * @param s
+   * @return
+   */
+  def composeMonads(t: Try[Int], o: Option[Int], e: Either[Throwable, Int], s: Seq[Int]): Int
 }
