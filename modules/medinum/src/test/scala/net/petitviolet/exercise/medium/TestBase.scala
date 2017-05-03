2afd182bf9499fed4dfb93a8dd97ce3461be0ef7
petitviolet <violethero0820@gmail.com>
2017-05-03 19:27:33 +0900

    future tests

--- modules/medinum/src/test/scala/net/petitviolet/exercise/medium/TestBase.scala
+++ modules/medinum/src/test/scala/net/petitviolet/exercise/medium/TestBase.scala
@@ -1,8 +1,10 @@
 package net.petitviolet.exercise.medium
 
-import org.scalatest.concurrent.TimeLimits
+import org.scalatest.concurrent.{ Futures, ScalaFutures, TimeLimits }
 import org.scalatest.{ FlatSpec, GivenWhenThen, Matchers }
+import org.scalatest.time._
 
-trait TestBase extends FlatSpec with Matchers with GivenWhenThen with TimeLimits {
+trait TestBase extends FlatSpec with Matchers with GivenWhenThen with TimeLimits with ScalaFutures {
   protected implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
+  implicit override val patienceConfig = PatienceConfig(timeout = Span(2, Seconds), interval = Span(20, Millis))
 }
